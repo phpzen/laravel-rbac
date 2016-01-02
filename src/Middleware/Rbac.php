131a4084565641b4605f159ed0d35793a00d5008
phpzen <szlek.marcin@gmail.com>
2016-01-02 10:28:17 +0100

    middleware

--- src/Middleware/Rbac.php
+++ src/Middleware/Rbac.php
@@ -15,7 +15,7 @@ class Rbac
      */
     public function handle($request, Closure $next, $level, $permission)
     {
-        echo config('auth.model');
+        echo config('auth.providers.users.model');
         return $next($request);
     }
 }

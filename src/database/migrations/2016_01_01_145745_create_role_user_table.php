263e22edd3b373a7a060e924fc753dc7017ef5d4
a_hovsep <hovsep@vezetvsem.ru>
2016-08-17 14:30:48 +0300

    Fix migrations foreign keys

--- src/database/migrations/2016_01_01_145745_create_role_user_table.php
+++ src/database/migrations/2016_01_01_145745_create_role_user_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
 
 class CreateRoleUserTable extends Migration
 {
@@ -14,8 +15,10 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('role_id')->unsigned()->index()->foreign()->references('id')->on('roles')->onDelete('cascade');
-            $table->integer('user_id')->unsigned()->index()->foreign()->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedInteger('role_id')->index();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->unsignedInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
